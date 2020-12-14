f04889def678f3455c350a1966686d8a994f233c
Artem Grankin <agrankin@klta.net>
2020-12-14 15:31:21 +0100

    remove old code for increment/decrement

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,13 +1,3 @@
-function reducer(state, action) {
-  if (action.type === 'INCREMENT') {
-    return state + action.amount;
-  } else if (action.type === 'DECREMENT') {
-    return state - action.amount;
-  } else {
-    return state;
-  }
-}
-
 function createStore(reducer) {
   let state = 0;
 
@@ -22,23 +12,3 @@ function createStore(reducer) {
     dispatch,
   };
 }
-
-const store = createStore(reducer);
-
-const incrementAction = {
-  type: 'INCREMENT',
-  amount: 3,
-};
-
-store.dispatch(incrementAction);
-console.log(store.getState()); // -> 3
-store.dispatch(incrementAction);
-console.log(store.getState()); // -> 6
-
-const decrementAction = {
-  type: 'DECREMENT',
-  amount: 4,
-};
-
-store.dispatch(decrementAction);
-console.log(store.getState()); // -> 2
