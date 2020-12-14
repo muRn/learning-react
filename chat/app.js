02ddd60a9f3a504e225072839f9d4cf4bcad9e8a
Artem Grankin <agrankin@klta.net>
2020-12-14 15:22:18 +0100

    add decrement action

--- chat/app.js
+++ chat/app.js
@@ -1,6 +1,8 @@
 function reducer(state, action) {
   if (action.type === 'INCREMENT') {
     return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
   } else {
     return state;
   }
@@ -16,3 +18,9 @@ const unknownAction = { type: 'UNKNOWN' };
 
 console.log(reducer(5, unknownAction)); // -> 5
 console.log(reducer(8, unknownAction)); // -> 8
+
+const decrementAction = { type: 'DECREMENT' };
+
+console.log(reducer(10, decrementAction)); // -> 9
+console.log(reducer(9, decrementAction)); // -> 8
+console.log(reducer(5, decrementAction)); // -> 4
