3a54bb2e45c11d21a852fbaf3e18a66222c6e324
Artem Grankin <agrankin@klta.net>
2020-12-14 16:52:31 +0100

    add ThreadTabs component

--- chat/src/App.js
+++ chat/src/App.js
@@ -58,9 +58,34 @@ class App extends React.Component {
     const threads = state.threads;
     const activeThread = threads.find((t) => t.id === activeThreadId);
 
+    const tabs = threads.map(t => ({
+      title: t.title,
+      active: t.id === activeThreadId,
+    }
+    ));
+
     return (
       <div className='ui segment'>
+        <ThreadTabs tabs={tabs} />
         <Thread thread={activeThread} />
+      </div>
+    );
+  }
+}
+
+class ThreadTabs extends React.Component {
+  render() {
+    const tabs = this.props.tabs.map((tab, index) => (
+      <div
+        key={index}
+        className={tab.active ? "active item" : "item"}
+      >
+        {tab.title}
+      </div>
+    ));
+    return (
+      <div className='ui top attached tabular menu'>
+        {tabs}
       </div>
     );
   }
