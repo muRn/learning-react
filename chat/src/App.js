cc514069e57bf37edff441e35bbde9ca5d6b9eb5
Artem Grankin <agrankin@klta.net>
2020-12-14 16:04:51 +0100

    changed message from string into object having text, id and timestamp

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import uuid from 'uuid';
 import { createStore } from 'redux';
 
 function reducer(state, action) {
   if (action.type === 'ADD_MESSAGE') {
+    const newMessage = {
+      text: action.text,
+      timestamp: Date.now(),
+      id: uuid.v4(),
+    };
     return {
-      messages: state.messages.concat(action.message),
+      messages: state.messages.concat(newMessage),
     };
   } else if (action.type === 'DELETE_MESSAGE') {
     return {
-      messages: [
-        ...state.messages.slice(0, action.index),
-        ...state.messages.slice(
-          action.index + 1, state.messages.length
-        ),
-      ],
+      messages: state.messages.filter((m) => (
+        m.id !== action.id
+      ))
     };
   } else {
     return state;
@@ -55,7 +58,7 @@ class MessageInput extends React.Component {
   handleSubmit = () => {
     store.dispatch({
       type: 'ADD_MESSAGE',
-      message: this.state.value,
+      text: this.state.value,
     });
     this.setState({
       value: '',
@@ -83,10 +86,10 @@ class MessageInput extends React.Component {
 }
 
 class MessageView extends React.Component {
-  handleClick = (index) => {
+  handleClick = (id) => {
     store.dispatch({
       type: 'DELETE_MESSAGE',
-      index: index,
+      id: id,
     });
   };
 
@@ -95,9 +98,12 @@ class MessageView extends React.Component {
       <div
         className='comment'
         key={index}
-        onClick={() => this.handleClick(index)}
+        onClick={() => this.handleClick(message.id)}
       >
-        {message}
+        <div className='text'>
+          {message.text}
+          <span className='metadata'>@{message.timestamp}</span>
+        </div>
       </div>
     ));
     return (
