d85480800c14485172552bb09fe87a23b0550a6f
Artem Grankin <agrankin@klta.net>
2020-12-14 16:03:13 +0100

    drop-in replace store from redux instead of custom implementation

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,26 +1,5 @@
 import React from 'react';
-
-function createStore(reducer, initialState) {
-  let state = initialState;
-  const listeners = [];
-
-  const subscribe = (listener) => (
-    listeners.push(listener)
-  );
-
-  const getState = () => (state);
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(l => l());
-  };
-
-  return {
-    subscribe,
-    getState,
-    dispatch,
-  };
-}
+import { createStore } from 'redux';
 
 function reducer(state, action) {
   if (action.type === 'ADD_MESSAGE') {
