5ab2fd80aadfbf65c1af13c4b6ba8853d9fcd172
Artem Grankin <agrankin@klta.net>
2020-12-14 15:23:11 +0100

    add amount to both actions

--- chat/app.js
+++ chat/app.js
@@ -1,26 +1,24 @@
 function reducer(state, action) {
   if (action.type === 'INCREMENT') {
-    return state + 1;
+    return state + action.amount;
   } else if (action.type === 'DECREMENT') {
-    return state - 1;
+    return state - action.amount;
   } else {
     return state;
   }
 }
 
-const incrementAction = { type: 'INCREMENT' };
+const incrementAction = {
+  type: 'INCREMENT',
+  amount: 5,
+};
 
-console.log(reducer(0, incrementAction)); // -> 1
-console.log(reducer(1, incrementAction)); // -> 2
-console.log(reducer(5, incrementAction)); // -> 6
+console.log(reducer(0, incrementAction)); // -> 5
+console.log(reducer(1, incrementAction)); // -> 6
 
-const unknownAction = { type: 'UNKNOWN' };
+const decrementAction = {
+  type: 'DECREMENT',
+  amount: 11,
+};
 
-console.log(reducer(5, unknownAction)); // -> 5
-console.log(reducer(8, unknownAction)); // -> 8
-
-const decrementAction = { type: 'DECREMENT' };
-
-console.log(reducer(10, decrementAction)); // -> 9
-console.log(reducer(9, decrementAction)); // -> 8
-console.log(reducer(5, decrementAction)); // -> 4
+console.log(reducer(100, decrementAction)); // -> 89
