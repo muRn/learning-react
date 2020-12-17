9145384011bb2e70ea4770a02cf0da9d846aad14
Artem Grankin <agrankin@klta.net>
2020-12-17 01:21:32 +0100

    Wrap App into WrappedApp to have redux store in context use new ThreadDisplay component in App Please enter the commit message for your changes. Lines starting

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 const reducer = combineReducers({
   activeThreadId: activeThreadIdReducer,
@@ -86,32 +87,12 @@ function messagesReducer(state = [], action) {
 
 const store = createStore(reducer);
 
-class App extends React.Component {
-  componentDidMount() {
-    store.subscribe(() => this.forceUpdate());
-  }
-
-  render() {
-    const state = store.getState();
-    const activeThreadId = state.activeThreadId;
-    const threads = state.threads;
-    const activeThread = threads.find((t) => t.id === activeThreadId);
-
-    const tabs = threads.map(t => ({
-      title: t.title,
-      active: t.id === activeThreadId,
-      id: t.id,
-    }
-    ));
-
-    return (
-      <div className='ui segment'>
-        <ThreadTabs tabs={tabs} />
-        <Thread thread={activeThread} />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className='ui segment'>
+    <ThreadTabs />
+    <ThreadDisplay />
+  </div>
+);
 
 const Tabs = (props) => (
   <div className='ui top attached tabular menu'>
@@ -261,4 +242,10 @@ class ThreadDisplay extends React.Component {
   }
 }
 
-export default App;
+const WrappedApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default WrappedApp;
